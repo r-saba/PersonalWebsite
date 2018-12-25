5815b37a41f1f722c364e6ad556cdc8cdc3cef51
RAGITH <r_sabapa@live.concordia.ca>
2018-12-24 22:28:20 -0500

    Added smooth scroll

--- WebSite1/JavaScript.js
+++ WebSite1/JavaScript.js
@@ -1,4 +1,20 @@
-﻿$(window).load(function() {
+﻿$(window).load(function () {
+    $(".navbar").delay(2000).fadeIn("slow");
 	$(".loader").delay(2000).fadeOut("slow");
-  $("#overlayer").delay(2000).fadeOut("slow");
+    $("#overlayer").delay(2000).fadeOut("slow");
 })
+
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 80
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
